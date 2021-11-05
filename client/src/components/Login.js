ac1888c8e31cc49b1ef026a6cbd3ef1d1636cf69
Kyson Briggs <kysonjbriggs@gmail.com>
2021-11-05 08:30:03 -0600

    Finished Register/Login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,83 +1,88 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import { NotificationManager } from "react-notifications";
 import { BsPersonBoundingBox } from "react-icons/bs";
 import { TiUserAdd } from "react-icons/ti";
 import "../css/Login.css";
+import { Formik, Form } from "formik";
+import { TextField } from "./TextField";
+import * as Yup from "yup";
 
 const Login = ({ setAuth }) => {
-  const [inputs, setInputs] = useState({
-    email: "",
-    password: "",
+  const validate = Yup.object({
+    email: Yup.string()
+      .email("* Email is invalid!")
+      .required("* Email is required"),
+    password: Yup.string()
+      .min(6, "* Password must be at least 6 characters")
+      .required("* Password is required"),
   });
-
-  const { email, password } = inputs;
-
-  const onChange = (e) => {
-    setInputs({ ...inputs, [e.target.name]: e.target.value });
-  };
-
-  const onSubmitForm = async (e) => {
-    e.preventDefault();
-    try {
-      const body = { email, password };
-
-      const response = await fetch("http://localhost:5000/auth/login", {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify(body),
-      });
-
-      const parseRes = await response.json();
-
-      if (parseRes.jwtToken) {
-        localStorage.setItem("token", parseRes.jwtToken);
-        setAuth(true);
-        NotificationManager.success("Logged in Successfully");
-      } else {
-        setAuth(false);
-        NotificationManager.error(parseRes);
-      }
-    } catch (err) {
-      console.log(err.message);
-    }
-  };
 
   return (
     <Fragment>
-      <div className="title">
-        <h1>
-          Login <BsPersonBoundingBox />{" "}
-        </h1>
-      </div>
-      <form className="logForm" onSubmit={onSubmitForm}>
-        <label for="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => onChange(e)}
-          required
-        />
-        <label for="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => onChange(e)}
-          required
-        />
-        <button>Submit</button>
-      </form>
-      <Link id="linkToRegister" to="/register">
-        Register <TiUserAdd />{" "}
-      </Link>
+      <Formik
+        validationSchema={validate}
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        onSubmit={async (values) => {
+          try {
+            let { email, password } = values;
+
+            const response = await fetch("http://localhost:5000/auth/login", {
+              method: "POST",
+              headers: {
+                "Content-type": "application/json",
+              },
+              body: JSON.stringify({ email, password }),
+            });
+
+            const parseRes = await response.json();
+
+            if (parseRes.jwtToken) {
+              localStorage.setItem("token", parseRes.jwtToken);
+              setAuth(true);
+              NotificationManager.success("Logged in Successfully");
+            } else {
+              setAuth(false);
+              NotificationManager.error(parseRes);
+            }
+          } catch (err) {
+            console.log(err.message);
+          }
+        }}
+      >
+        {(formik) => (
+          <div>
+            <div className="title">
+              <h1>
+                Login <BsPersonBoundingBox />{" "}
+              </h1>
+            </div>
+            <Form className="logForm">
+              <TextField
+                label="Email"
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email"
+              />
+              <TextField
+                label="Password"
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+              />
+              <button>Submit</button>
+            </Form>
+            <Link id="linkToRegister" to="/register">
+              Register <TiUserAdd />{" "}
+            </Link>
+          </div>
+        )}
+      </Formik>
     </Fragment>
   );
 };
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,112 +1,99 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import { NotificationManager } from "react-notifications";
-import '../css/Register.css'
-import { BsPersonBoundingBox } from 'react-icons/bs'
-import {BiLogInCircle} from 'react-icons/bi'
-import { Formik, Form } from 'formik';
-import * as Yup from 'yup'
+import "../css/Register.css";
+import { BsPersonBoundingBox } from "react-icons/bs";
+import { BiLogInCircle } from "react-icons/bi";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import { TextField } from "./TextField";
 
-
-const Register = ({setAuth}) => {
-  const [inputs, setInputs] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
-
-  const { username, email, password } = inputs;
-
-  const onChange = (e) => {
-    setInputs({ ...inputs, [e.target.name]: e.target.value });
-  };
-
-  const onSubmitForm = async (e) => {
-    e.preventDefault();
-
-    try {
-      const body = { username, email, password };
-
-      const response = await fetch("http://localhost:5000/auth/register", {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify(body),
-      });
-      const parseRes = await response.json();
-
-      if (parseRes.jwtToken) {
-        localStorage.setItem('token', parseRes.jwtToken)
-        setAuth(true);
-        NotificationManager.success('Register Successful')
-      } else {
-        setAuth(false);
-        NotificationManager.error(parseRes)
-      }
-    } catch (err) {
-      console.log(err.message);
-    }
-  };
-
+const Register = ({ setAuth }) => {
   const validate = Yup.object({
     username: Yup.string()
-      .max(20, 'Must be 20 characters or less')
-      .required('Required'),
+      .max(20, "* Username must be 20 characters or less")
+      .required("* Username is required"),
     email: Yup.string()
-      .email('Email is invalid!')
-      .required('Email is required'),
+      .email("* Email is invalid!")
+      .required("* Email is required"),
     password: Yup.string()
-      .min(6, 'Password must be at least 6 characters')
-      .required('Password is required')
-  })
-
-  
+      .min(6, "* Password must be at least 6 characters")
+      .required("* Password is required"),
+  });
 
   return (
     <Fragment>
       <Formik
         validationSchema={validate}
+        initialValues={{
+          username: "",
+          email: "",
+          password: "",
+        }}
+        onSubmit={async (values) => {
+          try {
+            let { username, email, password } = values;
+
+            const response = await fetch(
+              "http://localhost:5000/auth/register",
+              {
+                method: "POST",
+                headers: {
+                  "Content-type": "application/json",
+                },
+                body: JSON.stringify({ username, email, password }),
+              }
+            );
+            const parseRes = await response.json();
+
+            if (parseRes.jwtToken) {
+              localStorage.setItem("token", parseRes.jwtToken);
+              setAuth(true);
+              NotificationManager.success("Register Successful");
+            } else {
+              setAuth(false);
+              NotificationManager.error(parseRes);
+            }
+          } catch (err) {
+            console.log(err.message);
+          }
+        }}
       >
-        {formik => (
+        {(formik) => (
           <div>
-          <div className='title'>
-          <h1>Register <BsPersonBoundingBox /></h1>
-          </div>
-          <Form className='regForm' onSubmit={onSubmitForm}>
-            <label for="username">Username</label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => onChange(e)}
-            />
-            {formik.errors.username ? <div>{formik.errors.username}</div> : null}
-            <label for="email">Email</label>
-            <input
-              type="email"
-              id='email'
-              name="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => onChange(e)}
-            />
-            {formik.errors.email ? <div>{formik.errors.email}</div> : null}
-            <label for="password">Password</label>
-            <input
-              type="password"
-              id='password'
-              name="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => onChange(e)}
-            />
-            {formik.errors.password ? <div>{formik.errors.password}</div> : null}
-            <button>Submit</button>
-          </Form>
-          <Link className='link' to="/login"><BiLogInCircle /> Login</Link>
+            <div className="title">
+              <h1>
+                Register <BsPersonBoundingBox />
+              </h1>
+            </div>
+            <Form className="regForm">
+              <TextField
+                label="Username"
+                type="text"
+                id="username"
+                name="username"
+                placeholder="Username"
+              />
+              <TextField
+                label="Email"
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Email"
+                className="is-invalid"
+              />
+              <TextField
+                label="Password"
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+              />
+              <button>Submit</button>
+            </Form>
+            <Link className="link" to="/login">
+              <BiLogInCircle /> Login
+            </Link>
           </div>
         )}
       </Formik>
