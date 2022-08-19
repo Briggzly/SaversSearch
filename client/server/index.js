bc9e6c149c57ff1cd6aa7ce935731908df91ab2d
Kyson Briggs <kysonjbriggs@gmail.com>
2022-08-19 10:43:30 -0600

    testing a token

--- client/server/index.js
+++ client/server/index.js
@@ -2,30 +2,31 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const authorize = require("./middleware/authorize");
-const path = require("path")
-require("dotenv").config('./server')
+const path = require("path");
+require("dotenv").config("./server");
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.resolve(__dirname, './client/build')))
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 // routes
-app.use('/', require('./routes/jwtAuth'))
+app.use("/", require("./routes/jwtAuth"));
 
 app.use("/auth", require("./routes/jwtAuth"));
 
-app.use(authorize)
+app.use(authorize);
+
+// testing something
 
 app.use("/dashboard", require("./routes/dashboard"));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, './client/build', 'index.html'))
-})
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+});
 
-
-const {PORT} = process.env;
+const { PORT } = process.env;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
